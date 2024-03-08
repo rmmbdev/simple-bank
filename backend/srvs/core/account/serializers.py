631201c19fe1acad9aa0dd602d8b9c58d06d6537
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-08 14:43:48 +0330

    Add "UserSerializer" and "AccountSerializer"

---
+++ backend/srvs/core/account/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework.serializers import (
+    CurrentUserDefault,
+    ModelSerializer,
+    Serializer,
+)
+from backend.srvs.core.account.models import (
+    User,
+    Account,
+)
+
+
+class AccountSerializer(ModelSerializer):
+    class Meta:
+        model = Account
+        fields = [
+            "id",
+        ]
+        read_only_fields = [
+            "id"
+        ]
+
+
+class UserSerializer(ModelSerializer):
+    accounts = AccountSerializer(many=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+        ]
+        read_only_fields = [
+            "username"
+        ]
