6307e770db0c0dd98d8d2ecf73ce930beebd2b37
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-08 11:17:28 +0330

    Restructure project

--- backend/srvs/core/settings.py
+++ backend/srvs/core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'backend.srvs.core.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.srvs.core.wsgi.application'
 
 
 # Database
