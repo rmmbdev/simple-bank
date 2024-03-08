c63d262c0448542ac90f4c0805773f6bf3fc5888
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-08 16:41:21 +0330

    Refactor "Transaction" and add balance

--- backend/srvs/core/account/models.py
+++ backend/srvs/core/account/models.py
@@ -26,6 +26,9 @@ from django.db.models import (
     UUIDField,
     RESTRICT,
     DecimalField,
+    TextChoices,
+    QuerySet,
+    Sum,
 )
 from django.utils import (
     timezone,
@@ -85,6 +88,23 @@ class Account(BaseAbstractModel):
         db_index=True,
     )
 
+    @property
+    def balance(self):
+        in_value = self.in_transactions.aggregate(sum_value=Sum('amount'))
+        if (in_value is not None) and ("sum_value" in in_value) and (in_value["sum_value"] is not None):
+            in_value = in_value["sum_value"]
+        else:
+            in_value = 0
+
+        out_value = self.out_transactions.aggregate(sum_value=Sum('amount'))
+
+        if (out_value is not None) and ("sum_value" in out_value) and (out_value["sum_value"] is not None):
+            out_value = out_value["sum_value"]
+        else:
+            out_value = 0
+
+        return in_value - out_value
+
     def save(self, *args, **kwargs):
         adding = self._state.adding
 
@@ -93,27 +113,63 @@ class Account(BaseAbstractModel):
                 super().save(*args, **kwargs)
 
                 banker_account = Account.objects.filter(owner__username="banker").first()
-                Transaction.objects.create(
+                transaction = Transaction(
                     source=banker_account,
                     destination=self,
                     amount=INITIAL_AMOUNT,
+                    status=Transaction.Status.COMPLETED,
+                    type=Transaction.Type.INCREMENT,
                 )
+                transaction.save()
+
+    def increase_balance(self, amount):
+        banker_account = Account.objects.filter(owner__username="banker").first()
+        Transaction.objects.create(
+            source=banker_account,
+            destination=self,
+            amount=amount,
+            status=Transaction.Status.COMPLETED,
+            type=Transaction.Type.INCREMENT,
+        )
 
 
 class Transaction(BaseAbstractModel):
+    class Status(TextChoices):
+        PENDING: str = "PENDING"
+        COMPLETED: str = "COMPLETED"
+
+    class Type(TextChoices):
+        INCREMENT: str = "INCREMENT"
+        TRANSFER: str = "TRANSFER"
+
     source: ForeignKey[Account] = ForeignKey(
         to=Account,
         on_delete=RESTRICT,
-        related_name="out_transaction",
+        related_name="out_transactions",
         db_index=True,
     )
     destination: ForeignKey[Account] = ForeignKey(
         to=Account,
         on_delete=RESTRICT,
-        related_name="in_transaction",
+        related_name="in_transactions",
         db_index=True,
     )
     amount: DecimalField[Decimal] = DecimalField(
         max_digits=AMOUNT_MAX_DIGITS,
         decimal_places=AMOUNT_DECIMAL_PLACES,
     )
+    status: CharField[str] = CharField(
+        max_length=25,
+        choices=Status.choices,
+        default=Status.PENDING,
+        db_index=True,
+    )
+    type: CharField[str] = CharField(
+        max_length=25,
+        choices=Type.choices,
+        default=Type.INCREMENT,
+        db_index=True,
+    )
+
+    # def save(self, *args, **kwargs):
+    #     adding = self._state.adding
