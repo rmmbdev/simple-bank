f3cba2bfd5840140152321ebc40651c7236d7d18
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-08 14:44:16 +0330

    Add "ProfileViewSet"

--- backend/srvs/core/account/views.py
+++ backend/srvs/core/account/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+)
 
-# Create your views here.
+from rest_framework.viewsets import (
+    GenericViewSet,
+    ReadOnlyModelViewSet,
+)
+from backend.srvs.core.account.models import (
+    User,
+    Account,
+    Transaction,
+)
+from rest_framework.permissions import (
+    BasePermission,
+    IsAuthenticated,
+)
+
+from backend.srvs.core.account.serializers import UserSerializer
+
+
+class ProfileViewSet(
+    ListModelMixin,
+    GenericViewSet,
+):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return self.queryset.filter(id=user.id)
