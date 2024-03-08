3af0a0ec61b4409c474f2f9097cd832da8b9c9a9
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-08 14:26:49 +0330

    Add creating "banker" "User" and "Account" to migrations

--- backend/srvs/core/account/migrations/0001_initial.py
+++ backend/srvs/core/account/migrations/0001_initial.py
@@ -6,8 +6,15 @@ import uuid
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration):
+def add_data(apps, schema_editor):
+    user_model = apps.get_model('account', 'User')
+    user_model.objects.create(
+        username="banker",
+        password="g!s3@feR"
+    )
 
+
+class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
@@ -20,17 +27,29 @@ class Migration(migrations.Migration):
             fields=[
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('is_superuser', models.BooleanField(default=False,
+                                                     help_text='Designates that this user has all permissions without explicitly assigning them.',
+                                                     verbose_name='superuser status')),
                 ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
                 ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
                 ('email', models.EmailField(blank=True, max_length=254, verbose_name='email address')),
-                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('is_staff', models.BooleanField(default=False,
+                                                 help_text='Designates whether the user can log into this admin site.',
+                                                 verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True,
+                                                  help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
+                                                  verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('id', models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, primary_key=True,
+                                        serialize=False)),
                 ('username', models.CharField(max_length=50, unique=True)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(blank=True,
+                                                  help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                                                  related_name='user_set', related_query_name='user', to='auth.group',
+                                                  verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                                                            related_name='user_set', related_query_name='user',
+                                                            to='auth.permission', verbose_name='user permissions')),
             ],
             options={
                 'verbose_name': 'user',
@@ -41,4 +60,5 @@ class Migration(migrations.Migration):
                 ('objects', django.contrib.auth.models.UserManager()),
             ],
         ),
+        migrations.RunPython(add_data),
     ]
--- backend/srvs/core/account/migrations/0002_account.py
+++ backend/srvs/core/account/migrations/0002_account.py
@@ -7,8 +7,13 @@ from django.conf import settings
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration):
+def add_data(apps, schema_editor):
+    user_model = apps.get_model('account', 'User')
+    account_model = apps.get_model('account', 'Account')
+    account_model.objects.create(owner=user_model.objects.filter(username="banker").first())
 
+
+class Migration(migrations.Migration):
     dependencies = [
         ('account', '0001_initial'),
     ]
@@ -17,13 +22,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Account',
             fields=[
-                ('id', models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, primary_key=True,
+                                        serialize=False)),
                 ('created_at', models.DateTimeField(db_index=True, default=django.utils.timezone.now)),
                 ('modified_at', models.DateTimeField(auto_now=True, db_index=True)),
-                ('owner', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, related_name='accounts', to=settings.AUTH_USER_MODEL)),
+                ('owner', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, related_name='accounts',
+                                            to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'abstract': False,
             },
         ),
+        migrations.RunPython(add_data),
     ]
