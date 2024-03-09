a568188d1aed322137917b7d6af34badc383e186
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-09 11:25:27 +0330

    Add "Core" adapter to project

---
+++ backend/adapters/core/__init__.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class Core:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def validate_token(self, token):
+        url = self.base_url + "/api/token/verify/"
+        payload = {
+            "token": token,
+        }
+        response = requests.post(url=url, data=payload)
+        if response.ok:
+            return True
+        return False
