aa93a088e60deffaabd5475cab6aac5343d2dbfb
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-09 11:24:25 +0330

    Add "ACCESS_TOKEN_LIFETIME" to settings

--- backend/srvs/core/settings.py
+++ backend/srvs/core/settings.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from pathlib import Path
+
 from environs import (
     Env,
 )
@@ -101,6 +103,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
 }
 
 # Internationalization
