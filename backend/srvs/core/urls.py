e266594a6df696f548f7798e9e7fee97c2570b1f
Reza Mohammadi <rmm.dev.backend@gmail.com>
2024-03-09 11:25:08 +0330

    Add "TokenVerify" to api

--- backend/srvs/core/urls.py
+++ backend/srvs/core/urls.py
@@ -9,6 +9,7 @@ from rest_framework.routers import (
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 from backend.srvs.core.account.views import (
@@ -21,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include('auth.urls')),
 
 ]
